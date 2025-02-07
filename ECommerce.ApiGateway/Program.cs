0ac284b95a27fd4b0db70dccc5d0350c7651d1af
SANKUD0 <tacofauche@gmail.com>
2025-02-06 23:44:04 -0500

    Ajoutez des fichiers projet.

---
+++ ECommerce.ApiGateway/Program.cs
@@ -0,0 +1,34 @@
+
+namespace ECommerce.ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapReverseProxy();
+
+            app.Run();
+        }
+    }
+}
